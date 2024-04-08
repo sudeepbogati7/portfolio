372a170d5aa691fc791b47aff357f8cb0e0560e5
sudeepbogati7 <sudeepbogati977@gmail.com>
2024-04-08 16:45:51 +0545

    Modifications

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 $(window).on('load', function () {
-  gsap.to('#loader', 1, { y: "-100%" });
-  gsap.to('#loader', 1, { opacity: 0 });
-  gsap.to('#loader', 0, { display: "none", delay: 1 });
-  gsap.to('#header', 0, { display: "block", delay: 1 })
+  gsap.to('body', 1, { background: '#474647' })
+  gsap.to('.loader', 1, { y: "-100%" });
+  gsap.to('.loader', 1, { opacity: 0 });
+  gsap.to('.loader', 0, { display: "none", delay: 1 });
+  gsap.to('#header', 0, { display: "block", delay: 1, background: '#474647' })
   gsap.to('#navigation-content', 0, { display: "none" });
   gsap.to('#navigation-content', 0, { display: "flex", delay: 1 });
 })
